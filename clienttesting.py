cb863f1e3fb6883da3756c8cec45ce09eb3d7c63
matthewknecht <1800887@matthewknecht.local>
2015-05-18 21:47:45 -0500

    boop

---
+++ clienttesting.py
@@ -0,0 +1,28 @@
+import socket, sys, select
+def client():
+    s = socket.socket()
+    ip = socket.gethostname()
+    port = 9009
+    try:
+        s.connect((ip, port))
+        print "Connected!"
+    except:
+        print "Host not found! Goodbye!"
+    while True:
+        socket_list = [sys.stdin, s]
+        read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
+        for sock in read_sockets:
+            if sock == s:
+                msg = sock.recv(4096)
+                if not msg :
+                    print "Whoops! Something went wrong!"
+                    sys.exit()
+                else:
+                    sys.stdout.write(msg)
+                    sys.stdout.write('<You> '); sys.stdout.flush()
+            else:
+                msg = sys.stdin.readline()
+                s.send(msg)
+                sys.stdout.write('<You> '); sys.stdout.flush()
+if __name__ == "__main__":
+    sys.exit(client())
